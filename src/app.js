20f1ce9ef31846f28b6ab5092c6b89a6ef5410d8
Ayoubchelh1 <164335444+Ayoubchelh1@users.noreply.github.com>
2024-07-08 08:19:02 +0000

    First changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let primero of pronoun) {
+    for (let segundo of adj) {
+      for (let tercero of noun) {
+        console.log(primero + segundo + tercero + ".com");
+      }
+    }
+  }
 };
